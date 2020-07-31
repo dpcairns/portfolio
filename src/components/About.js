8996dbb3dd4fc547032a89290a7cc9a91c5609df
surelis segarra <sdarianne34@gmail.com>
2020-07-30 17:29:07 -0700

    added about me info and link to resume pdf

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import { Container, Header } from 'semantic-ui-react';
+import Pdf from '../Documents/Resume.pdf';
 
 export default class About extends Component {
     render() {
         return (
-            <div>
-                About
-            </div>
+            <Container text>
+                <Header className='aboutMe' as='h2'>About Me</Header>
+                <p className='about-container'>
+                In 2017 I moved from Puerto Rico due to Hurricane Maria. because of this I moved to Oregon to start a new life and during that process I made the greatest transition and started stufying software engineering
+                </p>
+                <p>
+                    As a frontend developer I am fascinated by expressing my creativity through beautiful functional apps. I enjoy working in collaborative enviroment and look to join a team to put my skills in planning, implementing, analyzing and problem solving to use.
+                </p>
+                <p>
+                    While I am not coding you can find me cooking delicious meals at home, watchiong The Office for the thousandth time or going on road trips.
+                </p>
+
+                <a classname='resume' href={Pdf}>Link to my Resume</a>
+            </Container>   
         )
     }
 }
