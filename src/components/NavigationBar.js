75403777320d75ef58ba11a01b6af1de41ad3e93
surelis segarra <sdarianne34@gmail.com>
2020-07-31 14:31:49 -0700

    created card I have to align it where I want it to go

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import logo from '../assets/logo.png';
 // import sun from '../assets/sun.png';
 // import moon from '../assets/moon.png';
-import { Menu, Image, Icon } from 'semantic-ui-react';
+import { Menu, Image } from 'semantic-ui-react';
 
     
  export default class NavigationBar extends Component {
--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
+import { Icon } from 'semantic-ui-react';
+import recipe from '../assets/recipe.png';
+
+
 
 export default class ProjectsCard extends Component {
     render() {
         return (
-            <div>
-                Project Card
+            <div className='card middle'>
+                <div className='front'>
+                    <img src={recipe} alt=''/>
+                </div>
+                <div className='back'>
+                    <div className='back-content middle'>
+                      <h2>Back</h2>
+                      <span>languages used</span>   
+                      <div className='icons'>
+                           <a href='#'><Icon name='github square'/></a>
+                           <a href='#'><Icon name='linkify'/></a>
+                           <a href='#'><Icon name='linkedin square'/></a>
+                           <a href='#'><Icon name='twitter square'/></a>
+                      </div>
+                    </div>
+                </div>
             </div>
         )
     }
