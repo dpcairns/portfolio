5a0b5da39a24f29f44f8e8f97930cbf0ab91563d
surelis segarra <sdarianne34@gmail.com>
2020-08-03 14:32:37 -0700

    added links to projects cards

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -13,7 +13,9 @@ export default class ProjectsCard extends Component {
     render() {
         return (
             <>
-                {/* <h1>Projects</h1> */}
+                <h1>Projects</h1>
+
+                {/* project 1 */}
                 <div className='project-body'>
                     <div className='project-post'>
                         <div className='project-img-container'>
@@ -21,15 +23,77 @@ export default class ProjectsCard extends Component {
                         </div>
                         <div className='project-info-container'>
                             <div className='project-info'>
-                            <span>languages</span>
-                            <span>languages</span>
-                            </div>
                             <h1 className='project-title'>React Slack Clone</h1>
+                            <span>React | Redux | Firebase | Semantic UI</span>
+                            </div>
                             <p className='project-text'>Personally developed a full-stack chat application from front to back with React,
-                            Redux, Firebase 5, and Semantic UI React. Allows user to communicate between
+                            Redux, Firebase 5, and Semantic UI React. React Slack Clone allows user to communicate between
                             development teams or groups (channels)</p>
-                            <a href='https://github.com/SuriSegarra'className='cta'><Icon name='github' ></Icon></a>
-                            <a href='https://github.com/SuriSegarra'className='cta'><Icon name='linkedin' ></Icon></a>
+                            <div>
+                            <a href='https://github.com/SuriSegarra/Slack-clone'className='cta'><Icon name='github' ></Icon></a>
+                            <a href='https://react-slack-622cb.web.app/login'className='cta'><Icon name='linkify' ></Icon></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* project 2 */}
+                <div className='project-body'>
+                    <div className='project-post'>
+                        <div className='project-img-container'>
+                            <img className='project-img' src={recipe} alt=''/>
+                        </div>
+                        <div className='project-info-container'>
+                            <div className='project-info'>
+                            <h1 className='project-title'>Just A Recipe</h1>
+                            <span>ReactJS | Node | Express | HTML | CSS | MongoDB | Mongoose | Firebase | Spoonacular API</span>
+                            </div>
+                            <p className='project-text'>In one week sprint my team I created a full stack website that uses React, NodeJS, Firebase, CSS3 and Spoonacular API to build a recipe site with over 350,000 recipes available for the user. The recipes are able to be called based on name, allergens and dietary.)</p>
+                            <div>
+                            <a href='https://github.com/Just-A-Recipe'className='cta'><Icon name='github' ></Icon></a>
+                            <a href='https://justarecipe.netlify.app/'className='cta'><Icon name='linkify' ></Icon></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* project 3 */}
+                <div className='project-body'>
+                    <div className='project-post'>
+                        <div className='project-img-container'>
+                            <img className='project-img' src={twitter} alt=''/>
+                        </div>
+                        <div className='project-info-container'>
+                            <div className='project-info'>
+                            <h1 className='project-title'>Let's Talk About it</h1>
+                            <span>Web App</span>
+                            <span>ReactJS | Node | Express | HTML | CSS | MongoDB | Mongoose</span>
+                            </div>
+                            <p className='project-text'>My team and I created a bot for the alchemy code lab community in one week sprint. Let's Talk About It its A backend twitter bot that that helps stop the stigma of mental health by sharing facts and questions to help spark conversation and reflection. It scrapes data about mental health from research papers and websites and then it sends automated tweets for approval via email weekly to the developers team.</p>
+                            <div>
+                            <a href='https://github.com/mental-health-twitterbot'className='cta'><Icon name='github' ></Icon></a>
+                            <a href='https://letstalkabout-react.herokuapp.com/'className='cta'><Icon name='linkify' ></Icon></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Project 4 */}
+                <div className='project-body'>
+                    <div className='project-post'>
+                        <div className='project-img-container'>
+                            <img className='project-img' src={salud} alt=''/>
+                        </div>
+                        <div className='project-info-container'>
+                            <div className='project-info'>
+                            <h1 className='project-title'>Salud!</h1>
+                            <span>React | Node | Cocktail DB API | CSS</span>
+                            </div>
+                            <p className='project-text'>Created a full stack app in one week sprint using ReactJS and NodeJS.  Salud! is a simple, easy to use app that uses the Cocktail DB API  to get access to over 500+  best and latest cocktail recipes. The cocktails are available to be called based on name.</p>
+                            <div>
+                            <a href='https://github.com/Salud-alchemy'className='cta'><Icon name='github' ></Icon></a>
+                            <a href='https://intense-woodland-75694.herokuapp.com/login'className='cta'><Icon name='linkify' ></Icon></a>
+                            </div>
                         </div>
                     </div>
                 </div>
