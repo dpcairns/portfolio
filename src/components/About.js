575bfb58a915881a3ce31a6c9f086efe6428b896
surelis segarra <sdarianne34@gmail.com>
2020-08-03 14:56:00 -0700

    fixed typos in summary

--- src/components/About.js
+++ src/components/About.js
@@ -13,10 +13,10 @@ export default class About extends Component {
                 <div className='inner-container'>
                     <div className='p-container'>
                         <p className='paragraph'>
-                            My name is Surelis Segarra. In 2017 I moved from Puerto Rico due to Hurricane Maria. Because of this I moved to Oregon to start a new life and during this process I made the greatest transition and started studying software engineering.
+                            My name is Surelis Segarra. In 2017 I moved from Puerto Rico due to Hurricane Maria. Because of this I moved to Oregon to start a new life and during this process I made the greatest transition and started studying software development.
                         </p>
                         <p className='paragraph'>
-                        As a front end engineer I am fascinated by expressing my cerativity through beautiful functional apps. I enjoy working in collaborative enviroments and look to join a team to put my skills in planning, implementing, analyzing and problem solving to use.
+                        As a front end developer I am fascinated by expressing my creativity through beautiful functional apps. I enjoy working in collaborative enviroments and look to join a team to put my skills in planning, implementing, analyzing and problem solving to use.
                         </p>
                         <p className='paragraph'>
                             While I am not coding you can find me cooking delicious meals at home, watching The Office for the thousandth time or going on road trips.
