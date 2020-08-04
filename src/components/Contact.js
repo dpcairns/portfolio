0eebb58d5c0112ab3ce4ab28050c8538a26e112f
surelis segarra <sdarianne34@gmail.com>
2020-08-04 11:16:46 -0700

    created static contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
-import { Icon } from 'semantic-ui-react';
+// import { Icon } from 'semantic-ui-react';
 
 export default class Contact extends Component {
+    state = {
+        name: '',
+        email: '',
+        message: ''
+      }
+
+      onNameChange(event) {
+          this.setState({ name: event.target.value })
+      }
+
+      onEmailChange(event) {
+          this.setState({ email: event.target.value })
+      }
+
+      onMessageChange(event) {
+          this.setState({ message: event.target.value })
+      }
+      
+      handleSubmit(event) {
+
+      }
+      
     render() {
+
         return (
             <div className='contact-container'>
                         {/* <div className='project-img-container'>
@@ -15,7 +38,21 @@ export default class Contact extends Component {
                             </div>
                             <p className='contact-text'>You can always contact me via any of the social platform at the bottom of the page. But a  solid contact form will never fail you!</p>
                             <div>
-
+                            <form id="contact-form" onSubmit={this.handleSubmit} method="POST">
+                                <div className="form-group">
+                                    <label htmlFor="name">Name</label>
+                                    <input type="text" className="form-control" value={this.state.name} onChange={this.onNameChange} />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="exampleInputEmail1">Email address</label>
+                                    <input type="email" className="form-control" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange} />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="message">Message</label>
+                                    <textarea className="form-control" rows="5" value={this.state.message} onChange={this.onMessageChange} />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Submit</button>
+                            </form>
                             </div>
                         </div>
                     </div>
