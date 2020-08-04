462ce219648b4707cbecc5c732db9e484bab6978
surelis segarra <sdarianne34@gmail.com>
2020-08-03 18:41:37 -0700

    have to put projects card smaller

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { Icon } from 'semantic-ui-react';
 
 export default class Contact extends Component {
     render() {
         return (
-            <div>
-                Contact
-            </div>
+            <div className='contact-container'>
+                        {/* <div className='project-img-container'>
+                            <img className='project-img' src={''} alt=''/>
+                        </div> */}
+                        <div className='project-info-container'>
+                            <div className='project-info'>
+                            <h2 className='project-title'>Contact Me</h2>
+                            {/* <span>React | Redux | Firebase | Semantic UI</span> */}
+                            </div>
+                            <p className='contact-text'>You can always contact me via any of the social platform at the bottom of the page. But a  solid contact form will never fail you!</p>
+                            <div>
+
+                            </div>
+                        </div>
+                    </div>
         )
     }
 }
