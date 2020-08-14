623edbc79ec34b2aea5696f2e71fa21d79585d4e
surelis segarra <sdarianne34@gmail.com>
2020-08-14 15:15:38 -0700

    fixed nav bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-import Home from './Components/Home';
+import Home from "./Components/Home";
+import ProjectsCard from "./Components/ProjectsCard";
 // import About from './components/About';
 // import Projects from './components/ProjectsCard';
-// import Contact from './components/Contact';
+import Contact from "./Components/Contact";
 // import Footer from './components/Footer';
+import logo from "./assets/logo.png";
 
 function App() {
   return (
-    <div className='app-container'>
-    <Router>
-      <Route exact path='/' component={Home}/>
-    </Router>
+    <div className="app-container">
+      <Router>
+        <nav>
+          <div className="logo">
+            <Link to="/">
+              <img className="logo-img" src={logo} alt="" />
+            </Link>
+          </div>
+          <div className="nav-wrapper">
+            <ul>
+              <li>
+                <Link to="/projects">Projects</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={ProjectsCard} />
+        <Route exact path="/contact" component={Contact} />
+      </Router>
     </div>
   );
 }
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,21 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
 // import sun from '../assets/sun.png';
 // import moon from '../assets/moon.png';
-import { Menu, Image } from 'semantic-ui-react';
+import { Link } from "react-router-dom";
 
-    
- export default class NavigationBar extends Component {
-
-        render() {
-            return (
-                <Menu secondary>
-                    <Image className='logo' src={logo}/>
-                </Menu>
-            )
-        }
-    }
-    
-
+export default class NavigationBar extends Component {
+  render() {
+    return (
+      <></>
+      //   <div className="nav-container">
+      //     <nav>
+      //       <div className="logo">
+      //         <Link to="/">
+      //           <img className="logo-img" src={logo} alt="" />
+      //         </Link>
+      //       </div>
+      //       <div className="nav-wrapper">
+      //         <ul>
+      //           <li>
+      //             <Link to="/projects">Projects</Link>
+      //           </li>
+      //           <li>
+      //             <Link to="/contact">Contact</Link>
+      //           </li>
+      //         </ul>
+      //       </div>
+      //     </nav>
+      //   </div>
+    );
+  }
+}
