644dc8d342d5cee97f092686f531958c9c4eb637
surelis segarra <sdarianne34@gmail.com>
2020-08-14 13:12:57 -0700

    done with footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React from "react";
+import { Icon } from "semantic-ui-react";
 
-export default class Footer extends Component {
-    render() {
-        return (
-            <div>
-                Footer
-            </div>
-        )
-    }
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <div>
+        <hr color="silver" className="line"></hr>
+      </div>
+      <span className="footer-text"> &copy; {year} Surelis Segarra</span>
+      <div className="footer-container">
+        <a href="https://github.com/SuriSegarra" className="contact-logo">
+          <Icon name="github"></Icon>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/surelis-segarra-bbbba3186/"
+          className="contact-logo"
+        >
+          <Icon name="linkedin alternate"></Icon>
+        </a>
+        <a href="https://twitter.com/surisegarra" className="contact-logo">
+          <Icon name="twitter square"></Icon>
+        </a>
+      </div>
+    </footer>
+  );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import NavigationBar from './NavigationBar';
-import About from './About';
-import ProjectsCard from './ProjectsCard';
-import Intro from './Intro';
-import Contact from './Contact';
+import React, { Component } from "react";
+import NavigationBar from "./NavigationBar";
+import About from "./About";
+import ProjectsCard from "./ProjectsCard";
+import Intro from "./Intro";
+import Contact from "./Contact";
+import Footer from "./Footer";
 
 export default class Home extends Component {
-    render() {
-        return (
-            <section>
-                <NavigationBar/>
-                
-                <Intro/>
+  render() {
+    return (
+      <section>
+        <NavigationBar />
 
-                <About/>
+        <Intro />
 
-                <ProjectsCard/>
-                
-                <Contact/>
-           </section>
+        <About />
 
-        
-                
-        )
-    }
+        <ProjectsCard />
+
+        <Contact />
+
+        <Footer />
+      </section>
+    );
+  }
 }
